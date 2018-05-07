6e217cd2eceb94593466227322ee9a3651ebe487
Sebastiaan Verbeek <a.j.s.verbeek@students.uu.nl>
2018-05-07 16:23:43 +0200

    Implemented Command()

--- STVRogue/GameLogic/Commands.cs
+++ STVRogue/GameLogic/Commands.cs
@@ -1,54 +1,82 @@
-﻿using System;
+﻿using STVRogue.GameLogic;
+using System;
+
 namespace STVRogue
 {
     public class Command
     {
         private string action;
-        public Command(string action) {
-            this.action = action;     
+        private Player player;
+        private Node node;
+
+        public Command (Node node, Player player) {
+            WritePossibleActionsToScreen();
+            action = Console.ReadLine();
+            this.player = player;
+            this.node = node;
         }
 
         public void executeCommand() {
-            if (action == "attack") {
+            if (action == "attack")
                 Attack();
-            }
-            else if (action == "item") {
-                WritePossibleItemUsage();
-                string item = Console.ReadLine();
-                if (item == "hp potion") {
-                    foreach (Item i in player.bag) {
-                        if (i.isHealingPotion()) {
-                            player.use(i);
-                            return;
-                        }
-                    }
-                }
-                else if (item == "crystal") {
-                    foreach (Item i in player.bag) {
-                        if (i.isCrystal()) {
-                            player.use(i);
-                            return;
-                        }
-                    }
-                }
-            }
+            else if (action == "item")
+                UseItem();
             else if (action == "flee") {
-                foreach (Node n in neighbors) {
-                    if (!n.packs.Any())
-                        player.location = n;
-                }
-                player.location = neighbors[0];
+                Flee();
             }
             else {
                 Console.WriteLine("Action not recognized.");
             }
         }
 
-        private static void Attack() {
-            foreach (Pack p in packs)
+        private void Attack() {
+            foreach (Pack p in node.packs)
                 foreach (Monster m in p.members) {
                     player.Attack(m);
                 }
+        }
+
+        private void UseItem() {
+            WritePossibleItemUsage();
+            string item = Console.ReadLine();
+            if (item == "hp potion")
+                useHPPotion();
+            else if (item == "crystal")
+                useCrystal();
+        }
+
+        private void useCrystal() {
+            foreach (Item i in player.bag) {
+                if (i.isCrystal()) {
+                    player.use(i);
+                    return;
+                }
+            }
+        }
+
+        private void useHPPotion() {
+            foreach (Item i in player.bag) {
+                if (i.isHealingPotion()) {
+                    player.use(i);
+                    return;
+                }
+            }
+        }
+
+        private void Flee() {
+            player.location = node.neighbors[0];
+        }
+
+        private void WritePossibleActionsToScreen() {
+            Console.WriteLine("Perform an action by typing: ");
+            Console.WriteLine("1. attack");
+            Console.WriteLine("2. item");
+            Console.WriteLine("3. flee");
+        }
+        private static void WritePossibleItemUsage() {
+            Console.WriteLine("Use an item by writing: ");
+            Console.WriteLine("a. hp potion");
+            Console.WriteLine("b. magic crystal");
         }
     }
 }
