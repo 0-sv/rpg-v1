113d3bb34c3558fefa3dce22e1073aa8c750da40
Jesse van den Berg <j.s.vandenberg3@students.uu.nl>
2018-05-11 18:06:00 +0200

    2 nieuwe tests gemaakt

--- XunitTests/XTest_Game.cs
+++ XunitTests/XTest_Game.cs
@@ -24,5 +24,54 @@ namespace STVRogue.GameLogic
 		{
             Assert.Throws<GameCreationException>(() => new Game(5, 2, 300));
 		}
+
+        [Fact]
+        public void XTest_disconnect_nodes()
+        {
+            Node node1 = new Node("1");
+            Node node2 = new Node("2");
+            Predicates p = new Predicates();
+            node1.Connect(node2);
+            Assert.True(p.isReachable(node1, node2));
+            node1.Disconnect(node2);
+            Assert.False(p.isReachable(node1, node2));
+        }
+
+        [Fact]
+        public void XTest_shortest_path()
+        {
+            
+            Node node1 = new Node("1");
+            Node node2 = new Node("2");
+            Node node3 = new Node("3");
+            Node node4 = new Node("4");
+            Node node5 = new Node("5");
+            Node node6 = new Node("6");
+            Node node7 = new Node("7");
+            node1.Connect(node2);
+            node2.Connect(node3);
+            node3.Connect(node4);
+            node4.Connect(node5);
+            node1.Connect(node6);
+            node6.Connect(node7);
+            node7.Connect(node5);
+            Dungeon d = new Dungeon(1, 2);
+            d.nodeList = new List<Node>() { node1, node2, node3, node4, node5, node6, node7 };
+            Assert.Equal(d.Shortestpath(node1, node5), new List<Node>() { node1, node6, node7, node5 });
+        }
+
+        [Fact]
+        public void XTest_shortest_path_unreachable()
+        {
+            Node node1 = new Node("1");
+            Node node2 = new Node("2");
+            Node node3 = new Node("3");
+            Node node4 = new Node("4");
+            node1.Connect(node2);
+            node3.Connect(node4);
+            Dungeon d = new Dungeon(1, 2);
+            d.nodeList = new List<Node>() { node1, node2, node3, node4 };
+            Assert.Equal(d.Shortestpath(node1, node4), new List<Node>() { node1 });
+        }
 	}
 }
