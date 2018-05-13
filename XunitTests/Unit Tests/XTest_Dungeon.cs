28a660d3c189ee75ae72134e7e3a35900890060a
Jesse van den Berg <j.s.vandenberg3@students.uu.nl>
2018-05-13 16:14:44 +0200

    Test added for Pack.Move and bug in Pack.Move fixed

--- XunitTests/Unit Tests/XTest_Dungeon.cs
+++ XunitTests/Unit Tests/XTest_Dungeon.cs
@@ -33,5 +33,21 @@ namespace STVRogue.GameLogic
 			}
 			
 		}
+
+        [Fact]
+        public void XTest_pack_move()
+        {
+            Dungeon dungeon = new Dungeon(5, 2);
+            Node node = new Node("testnode");
+            node.Connect(dungeon.nodeList[1]);
+            dungeon.nodeList.Add(node);
+            Pack pack = new Pack("testpack", 2);
+            pack.location = dungeon.nodeList[1];
+            pack.dungeon = dungeon;
+            dungeon.nodeList[1].packs = new List<Pack>() { pack };
+            pack.Move(node);
+            Assert.Contains(pack, node.packs);
+            Assert.DoesNotContain(pack, dungeon.nodeList[1].packs);
+        }
 	}
 }
