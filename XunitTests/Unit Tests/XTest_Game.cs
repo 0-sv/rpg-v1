e2efc56fbf0a17bb3d66b36e9924fc414678fe4e
BramT21 <36609057+BramT21@users.noreply.github.com>
2018-05-19 18:44:51 +0200

    shortestpath automated test

--- XunitTests/Unit Tests/XTest_Game.cs
+++ XunitTests/Unit Tests/XTest_Game.cs
@@ -112,50 +112,6 @@ namespace STVRogue.GameLogic
             Assert.False(p.isReachable(node1, node2));
         }
 
-        [Fact]
-        public void XTest_shortest_path()
-        {
-            
-            Node node1 = new Node("1");
-            Node node2 = new Node("2");
-            Node node3 = new Node("3");
-            Node node4 = new Node("4");
-            Node node5 = new Node("5");
-            Node node6 = new Node("6");
-            Node node7 = new Node("7");
-            node1.Connect(node2);
-            node2.Connect(node3);
-            node3.Connect(node4);
-            node4.Connect(node5);
-            node1.Connect(node6);
-            node6.Connect(node7);
-            node7.Connect(node5);
-            Dungeon d = new Dungeon(1, 2);
-            d.nodeList = new List<Node>() { node1, node2, node3, node4, node5, node6, node7 };
-            Assert.Equal(d.Shortestpath(node1, node5), new List<Node>() { node1, node6, node7, node5 });
-        }
-
-        [Fact]
-        public void XTest_shortest_path_unreachable()
-        {
-            Node node1 = new Node("1");
-            Node node2 = new Node("2");
-            Node node3 = new Node("3");
-            Node node4 = new Node("4");
-            node1.Connect(node2);
-            node3.Connect(node4);
-            Dungeon d = new Dungeon(1, 2);
-            d.nodeList = new List<Node>() { node1, node2, node3, node4 };
-            Assert.Equal(d.Shortestpath(node1, node4), new List<Node>() { node1 });
-        }
-		[Fact]
-		public void emptypath()
-		{
-			Dungeon d = new Dungeon(1, 2);
-			Node node1 = new Node("1");
-			d.nodeList = new List<Node>() { node1 };
-			List<Node> l = d.Shortestpath(node1, node1);
-			Assert.Empty(l);
-		}
+     
 	}
 }
